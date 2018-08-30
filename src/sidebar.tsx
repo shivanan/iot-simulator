6a0f583559beb78cd7f5849c6f7d5080c88255a0
Udhayakumar <udhaya@ecyber.com>
2018-08-30 19:22:40 +0530

    side bar collapse

--- src/sidebar.tsx
+++ src/sidebar.tsx
@@ -1,25 +1,40 @@
 import * as React from 'react';
 import { IDevice } from './device';
-export interface ISideBarProps {
+import { BottomBar } from './bottombar';
+
+//import { SidebarCollapse } from './sidebar_collapse';
+
+
+interface ISideBarProps {
     onAddDevice:(device:IDevice) => void;
 }
-export interface ISideBarState {
-    
+
+interface ISideBarState {
+    collapse: boolean;
 }
 
-
-
-
-
-
 export class SideBar extends React.Component<ISideBarProps,ISideBarState> {
-    constructor(props:ISideBarProps) {
+    constructor(props:ISideBarProps) {     
         super(props);
-        this.state = {}
+       
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            collapse: false,
+          };
     }
     addDevice(d:IDevice) {
         this.props.onAddDevice(d);
     }
+
+   
+   
+
+    toggle() {
+        this.setState({ collapse: !this.state.collapse });
+    }
+    
+
+
     render() {
         const numbers = ["T1.BLDG01.FL23", "T1.BLDG01.FL23", "T2.BLDG01.FL23", "T2.BLDG01.FL23"];        
         const listItems = numbers.map((numbers) =>
@@ -35,13 +50,13 @@ export class SideBar extends React.Component<ISideBarProps,ISideBarState> {
         
         
 
-        return <div className='sidebar' >
-            <div onClick={this.addDevice.bind(this,{'id':new Date()+'',type:'temperature'})}>Temp</div>
+        return   <div className='sidebar' style={{width: this.state.collapse ? '80px' : '300px'}} >
+            <div onClick={this.addDevice.bind(this,{'id':new Date()+'',type:'temperature'})} style={{display: this.state.collapse ? 'none' : 'block'}} >Temp</div>
             <div className="sensors-top">
-				<h5>Sensors</h5>
-				<div className="sensors-icon"></div>
-			</div>
-            <div className="sensors-search">
+				<h5 style={{display: this.state.collapse ? 'none' : 'block'}}>Sensors</h5>               
+				<div className="sensors-icon" onClick={this.toggle}  style={{position: this.state.collapse ? 'absolute' : 'inherit', top: this.state.collapse ? '46%' : '0'}}></div>
+			</div> 
+            <div className="sensors-search" style={{display: this.state.collapse ? 'none' : 'block'}} >
                 <input type="text" placeholder="Start Typing" id="myInput" />
                 <div className="search-chkbox-cont">
 						<div className="search-chkbox">
@@ -51,16 +66,16 @@ export class SideBar extends React.Component<ISideBarProps,ISideBarState> {
 						</div>						
 					</div>
                 <ul className="sensors-search-list">{listItems}</ul>
-            </div>
+            </div>         
 
-            
-
-            <div id="mount-point">
+            <div id="mount-point" style={{display: this.state.collapse ? 'none' : 'block'}} >
                 <div className="sensors-recent">
                     <h6>Recent</h6>
                     <ul>{sensors_recent_list}</ul>
                 </div>
             </div>
-        </div>;
+        </div>
+     
+        ;
     }
 }
